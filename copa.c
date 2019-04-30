9ecc3e66da5d6ac495e652eff515391e08ba997c
Santiago Locatelli <49878083+Lochaa@users.noreply.github.com>
2019-04-30 11:57:17 -0300

    Add files via upload

---
+++ copa.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "laberinto.h"
+#include <math.h>
+
+#define TOTAL_OBSTACULOS 3
+#define TOTAL_AYUDAS 6
+#define TAMANIO 15
+#define PARED '#'
+#define VACIO ' '
+#define IZQUIERDA 'a'
+#define DERECHA 'd'
+#define ARRIBA 'w'
+#define ABAJO 's'
+#define COPA 'C'
+#define JUGADOR 'J'
+#define RIVAL 'G'
+#define POCIONES 'P'
+#define IMPEDIMENTA 'I'
+#define RIDDIKULUS 'R'
+#define ESCREGUTO 'E'
+#define ACROMANTULA 'A'
+#define BOGGART 'B'
+#define ESFINGE 'F'
+#define DANIO_ESCREGUTO 20
+#define DANIO_ACROMANTULA 10
+#define DANIO_BOGGART 15
+#define CURACION 15
+#define DISTANCIA_MANHATTAN 10
+
+//RESGISTROS...
+
+typedef struct coordenada {
+    int fil;
+    int col;
+} coordenada_t;
+
+typedef struct obstaculo {
+    char codigo;
+    coordenada_t posicion;
+    int danio;
+} obstaculo_t;
+
+typedef struct ayuda {
+    char codigo;
+    coordenada_t posicion;
+    int vida_a_recuperar;
+} ayuda_t;
+
+typedef struct jugador {
+    int vida;
+    coordenada_t posicion;
+    int tope_ayudas;
+    ayuda_t ayudas[TOTAL_AYUDAS];
+} jugador_t;
+
+typedef struct rival {
+    coordenada_t posicion;
+    char direccion;
+    int cantidad_pasos;
+} rival_t;
+
+typedef struct copa {
+    char codigo;
+    coordenada_t posicion;
+} copa_t;
+
+typedef struct juego{
+    char laberinto_original[TAMANIO][TAMANIO];
+    jugador_t jugador;
+    rival_t rival;
+    copa_t copa;
+    obstaculo_t obstaculos[TOTAL_OBSTACULOS];
+    int tope_obstaculos;
+    ayuda_t ayudas[TOTAL_AYUDAS];
+    int tope_ayudas;
+} juego_t;
+
+//FUNCIONES...
+
+coordenada_t posicion_aleatoria(){
+	coordenada_t aleatoria;
+	srand ( (unsigned)time(NULL) );
+	aleatoria.col = rand()%TAMANIO;
+	aleatoria.fil = rand()%TAMANIO;
+
+	return aleatoria;
+}
+
+bool posicion_valida ( char laberinto[TAMANIO][TAMANIO], coordenada_t posicion_aleatoria ){
+
+	bool es_valida = false;
+	if ( laberinto[posicion_aleatoria.fil][posicion_aleatoria.col] == VACIO ){
+		es_valida = true;
+	}
+	return es_valida;
+}
+
+coordenada_t asignar_posicion ( char laberinto[TAMANIO][TAMANIO] ){
+
+	coordenada_t posicion;
+
+	posicion = posicion_aleatoria();
+
+	while ( !posicion_valida( laberinto, posicion ) ){
+		posicion = posicion_aleatoria();
+	}
+	return posicion; 
+}
+
+void mostrar_laberinto( char laberinto[TAMANIO][TAMANIO] ){
+	for ( int fila = 0; fila < TAMANIO; fila++ ){
+		for ( int columna = 0; columna < TAMANIO; columna++ ){
+			printf( "%c ", laberinto[fila][columna] );
+		}
+		printf("\n");
+	}
+}
+
+void inicializar_obstaculos ( juego_t* juego, char laberinto_copia[TAMANIO][TAMANIO] ){
+	char obstaculos[TOTAL_OBSTACULOS] = { ESCREGUTO, ACROMANTULA, BOGGART};
+	int danio_obstaculos[TOTAL_OBSTACULOS] = { DANIO_ESCREGUTO, DANIO_ACROMANTULA, DANIO_BOGGART};
+
+	juego->tope_obstaculos = TOTAL_OBSTACULOS;
+
+	for ( int i = 0; i < TOTAL_OBSTACULOS; i++ ){
+	juego->obstaculos[i].posicion = asignar_posicion ( laberinto_copia );
+	juego->obstaculos[i].danio = danio_obstaculos[i];
+	juego->obstaculos[i].codigo = obstaculos[i];
+	laberinto_copia [juego->obstaculos[i].posicion.fil][juego->obstaculos[i].posicion.col] = obstaculos[i];
+	}
+}
+
+void inicializar_ayudas ( juego_t* juego , char laberinto_copia[TAMANIO][TAMANIO] ){
+	char ayudas[TOTAL_AYUDAS] = { IMPEDIMENTA, RIDDIKULUS, POCIONES, POCIONES, POCIONES, ESFINGE};
+
+	juego->tope_ayudas = TOTAL_AYUDAS;
+
+	for ( int i = 0; i < TOTAL_AYUDAS; i++ ){
+		juego->ayudas[i].posicion = asignar_posicion ( laberinto_copia );
+		juego->ayudas[i].codigo = ayudas[i];
+		laberinto_copia [juego->ayudas[i].posicion.fil][juego->ayudas[i].posicion.col] = ayudas[i];
+
+		if ( ayudas[i] == POCIONES ){
+			juego->ayudas[i].vida_a_recuperar = CURACION;
+		}
+	}
+}
+
+bool cumple_distancia_manhattan ( coordenada_t coordenada_uno, coordenada_t coordenada_dos ){
+	bool cumple = false;
+	int distancia_filas =  coordenada_uno.fil - coordenada_dos.fil;
+	int distancia_columnas = coordenada_uno.col - coordenada_dos.col;
+
+	if ( abs( distancia_filas ) + abs( distancia_columnas ) > DISTANCIA_MANHATTAN ){
+		cumple = true;
+	}
+	return cumple;
+}
+
+void inicializar_participantes ( juego_t* juego, char laberinto_copia[TAMANIO][TAMANIO] ){
+	juego->rival.posicion = asignar_posicion ( laberinto_copia );
+
+	while ( !cumple_distancia_manhattan ( juego->copa.posicion, juego->rival.posicion ) ){
+		juego->rival.posicion = asignar_posicion ( laberinto_copia );
+	}
+	laberinto_copia [juego->rival.posicion.fil][juego->rival.posicion.col] = RIVAL;
+
+	juego->jugador.posicion = asignar_posicion ( laberinto_copia );
+
+	while ( !cumple_distancia_manhattan ( juego->copa.posicion, juego->jugador.posicion ) ){
+		juego->jugador.posicion = asignar_posicion ( laberinto_copia );		
+	}
+	laberinto_copia [juego->jugador.posicion.fil][juego->jugador.posicion.col] = JUGADOR;	
+}
+
+void inicializar_laberinto( juego_t* juego ){
+	char laberinto_copia[TAMANIO][TAMANIO];
+
+	inicializar_paredes_laberinto ( juego->laberinto_original );
+
+	for ( int fila = 0; fila < TAMANIO; fila++ ){
+		for ( int columna = 0; columna < TAMANIO; columna++ ){
+			laberinto_copia[fila][columna] = juego->laberinto_original[fila][columna];
+		}
+	}
+
+	juego->copa.posicion = asignar_posicion ( laberinto_copia );
+	laberinto_copia [juego->copa.posicion.fil][juego->copa.posicion.col] = COPA;
+
+	inicializar_obstaculos ( juego, laberinto_copia );
+
+	inicializar_ayudas ( juego, laberinto_copia );
+
+	inicializar_participantes ( juego, laberinto_copia );
+}
+
+void mover_rival(juego_t* juego){
+
+}
